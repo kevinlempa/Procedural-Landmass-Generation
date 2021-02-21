787ac511065bfc690c8b050beb89e7f2d73718db
Kevin Lempa <kevin.lempa@student.forsbergsskola.se>
2021-02-21 13:46:31 +0100

    adds Noise Visualization

---
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,20 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor {
+    [CustomEditor(typeof(MapGenerator))]
+    public class MapGeneratorEditor : UnityEditor.Editor {
+        public override void OnInspectorGUI() {
+            MapGenerator mapGen = (MapGenerator)target;
+            if (DrawDefaultInspector()) {
+                if (mapGen.autoUpdate) {
+                    mapGen.GenerateMap();
+                }
+            }
+
+            if (GUILayout.Button(("Generate"))) {
+                mapGen.GenerateMap();
+            }
+        }
+    }
+}
